a1ee73888c27c26888ff35d55c0ce0c1534da81f
Claudiu Andrei Nechitescu <clne@floridauniversitaria.es>
2020-12-17 16:54:41 +0100

    Añadiendo programa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,196 @@ namespace AP7
     {
         static void Main(string[] args)
         {
-            Program.Ej1();
+            Console.Write("Elige el ejercicio: ");
+            int ejercicio = int.Parse(Console.ReadLine());
+            switch (ejercicio)
+            {
+                case 1:
+                    {
+                        Program.Ej1();
+                        break;
+                    }
+                case 2:
+                    {
+                        Program.Ej2();
+                        break;
+                    }
+                case 3:
+                    {
+                        Program.Ej3();
+                        break;
+                    }
+                case 4:
+                    {
+                        Program.Ej4();
+                        break;
+                    }
+                case 5:
+                    {
+                        Program.Ej5();
+                        break;
+                    }
+                case 6:
+                    {
+                        Program.Ej6();
+                        break;
+                    }
+                case 7:
+                    {
+                        Program.Ej7();
+                        break;
+                    }
+                case 8:
+                    {
+                        Program.Ej8();
+                        break;
+                    }
+                case 9:
+                    {
+                        Program.Ej9();
+                        break;
+                    }
+                case 10:
+                    {
+                        Program.Ej10();
+                        break;
+                    }
+                case 11:
+                    {
+                        Program.Ej11();
+                        break;
+                    }
+                case 12:
+                    {
+                        Program.Ej12();
+                        break;
+                    }
+                case 13:
+                    {
+                        Program.Ej13();
+                        break;
+                    }
+                case 14:
+                    {
+                        Program.Ej14();
+                        break;
+                    }
+                case 15:
+                    {
+                        Program.Ej15();
+                        break;
+                    }
+                case 16:
+                    {
+                        Program.Ej16();
+                        break;
+                    }
+                case 17:
+                    {
+                        Program.Ej17();
+                        break;
+                    }
+                case 18:
+                    {
+                        Program.Ej18();
+                        break;
+                    }
+                case 19:
+                    {
+                        Program.Ej19();
+                        break;
+                    }
+                case 20:
+                    {
+                        Program.Ej20();
+                        break;
+                    }
+                case 21:
+                    {
+                        Program.Ej21();
+                        break;
+                    }
+                case 22:
+                    {
+                        Program.Ej22();
+                        break;
+                    }
+                case 23:
+                    {
+                        Program.Ej23();
+                        break;
+                    }
+                case 24:
+                    {
+                        Program.Ej24();
+                        break;
+                    }
+                case 25:
+                    {
+                        Program.Ej25();
+                        break;
+                    }
+                case 26:
+                    {
+                        Program.Ej26();
+                        break;
+                    }
+                case 27:
+                    {
+                        Program.Ej27();
+                        break;
+                    }
+                case 28:
+                    {
+                        Program.Ej28();
+                        break;
+                    }
+                case 29:
+                    {
+                        Program.Ej29();
+                        break;
+                    }
+                case 30:
+                    {
+                        Program.Ej30();
+                        break;
+                    }
+                case 31:
+                    {
+                        Program.Ej31();
+                        break;
+                    }
+                case 32:
+                    {
+                        Program.Ej32();
+                        break;
+                    }
+                case 33:
+                    {
+                        Program.Ej33();
+                        break;
+                    }
+                case 34:
+                    {
+                        Program.Ej34();
+                        break;
+                    }
+                case 35:
+                    {
+                        Program.Ej35();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Debes elegir un ejercicio del 1 al 35.");
+                        break;
+                    }
+            }
         }
         public static void Ej1()
         {
             Console.Write("Introduce un número del 1 al 10: ");
-            int numero = Convert.ToInt32(Console.ReadLine());
+            int numero = int.Parse(Console.ReadLine());
             switch (numero)
             {
                 case 1:
@@ -81,6 +266,138 @@ namespace AP7
                 Console.WriteLine("Estas frases son iguales");
             }
             Console.ReadKey();
+        }
+        public static void Ej3()
+        {
+
+        }
+        public static void Ej4()
+        {
+
+        }
+        public static void Ej5()
+        {
+
+        }
+        public static void Ej6()
+        {
+
+        }
+        public static void Ej7()
+        {
+
+        }
+        public static void Ej8()
+        {
+
+        }
+        public static void Ej9()
+        {
+
+        }
+        public static void Ej10()
+        {
+
+        }
+        public static void Ej11()
+        {
+
+        }
+        public static void Ej12()
+        {
+
+        }
+        public static void Ej13()
+        {
+
+        }
+        public static void Ej14()
+        {
+
+        }
+        public static void Ej15()
+        {
+
+        }
+        public static void Ej16()
+        {
+
+        }
+        public static void Ej17()
+        {
+
+        }
+        public static void Ej18()
+        {
+
+        }
+        public static void Ej19()
+        {
+
+        }
+        public static void Ej20()
+        {
+
+        }
+        public static void Ej21()
+        {
+
+        }
+        public static void Ej22()
+        {
+
+        }
+        public static void Ej23()
+        {
+
+        }
+        public static void Ej24()
+        {
+
+        }
+        public static void Ej25()
+        {
+
+        }
+        public static void Ej26()
+        {
+
+        }
+        public static void Ej27()
+        {
+
+        }
+        public static void Ej28()
+        {
+
+        }
+        public static void Ej29()
+        {
+
+        }
+        public static void Ej30()
+        {
+
+        }
+        public static void Ej31()
+        {
+
+        }
+        public static void Ej32()
+        {
+
+        }
+        public static void Ej33()
+        {
+
+        }
+        public static void Ej34()
+        {
+
+        }
+        public static void Ej35()
+        {
+
         }
     }
 }
